f662cc5e7fc8cf7c2da094df1dc6f09ad44b4e04
naveenprod <naveenmca18@gmail.com>
2017-05-26 16:35:37 +0530

    change for pull request

--- src/main/java/com/gitrequest/rest/dao/GitDao.java
+++ src/main/java/com/gitrequest/rest/dao/GitDao.java
@@ -3,7 +3,7 @@ package com.gitrequest.rest.dao;
 import java.util.List;
 
 import com.gitrequest.rest.bo.GitData;
-
+//Dao Interface
 public interface GitDao {	
 	public boolean storeGitPullData(List<GitData> user);
 	public String fetchGitStateData();
